fccf7fc8850be9926e3926df4ee3ec9c1aa0eee2
superwzx <wzx175@gmail.com>
2017-02-13 18:23:38 +0800

    update

--- src/jquery.wslides.js
+++ src/jquery.wslides.js
@@ -1,78 +1,74 @@
 /**
- * j@uery Slides Plugin
- * author: superwzx <wzx175@gmail.com>
+ * wSlider is a jQuery Slider Plugin
+ * author: wzx175@gmail.com
  **/
+
 (function($) {
 
-	var pluginName = "wslides",
+	var pluginName = "wSlider",
 
 		defaults = {
-			// 默认幻灯片的宽度
-			width:  'auto',
-			// 默认幻灯片的高度
+			// Slider's width
+			width: 'auto',
+
+			// Slider's height
 			height: 'auto',
-			// 默认第一个幻灯片开始
-			start:  1,
-			// 默认导航设置
-			navigation: {
-				active: true
-			},
-			// 默认分页设置
-			pagination: {
-				active: true
-			},
+
+			// start index
+			start: 1,
+
+			navigation: true,
+
+			pagination: true,
+
 			// 默认播放设置
 			play: {
-				active:       true,
-				interval:     5000,
-				auto:         false,
-				swap:         false,
+				active: true,
+				interval: 3000,
+				auto: false,
+				swap: false,
 				restartDelay: 2500
 			},
+
 			currentEffect: 'slide',
+
 			// 默认动画效果设置
 			effectOptions: {
 				slide: {
 					speed: 500
 				},
 				fade: {
-					speed:     300,
-					crossfade: true
+					speed: 300,
+					crossFade: true
 				}
 			},
+
 			// 默认回调
 			callback: {
-				loaded:   function() { },
-				start:    function() { },
-				complete: function() { }
+				loaded: function () { },
+				start: function () { },
+				complete: function () { }
 			}
 		};
 
-	
-	/*****************************************
-	 ************** 插件构造函数 ***************
-	 *****************************************/
 	/**
-	 * Plugin的构造函数
-	 * @param element {dom-obj} 幻灯片容器
-	 * @Param options {obj} 用户自定义设置
+	 * 插件构造函数
+	 * @param el {object} 幻灯片容器dom
+	 * @param options {object} 用户自定义设置
 	 * @return this
 	 **/
 	function Plugin (el, options) {
 		this.el = $(el);
 		this.options = $.extend(true, {}, defaults, options);
-		this._name = pluginName;
+		this.name = pluginName;
 		this.init();
 	}
 
 
-	/*****************************************
-	 *************** 插件方法 *****************
-	 *****************************************/
 	/**
 	 * Plugin.vendorPrefix
 	 * 检测浏览器，获取浏览器前缀
-	 * @return ["Moz", "Webkit", "Khtml", "O", "ms"].[i]
+	 * @return vendorName
 	 **/
 	Plugin.vendorPrefix = (function() {
 		var body = document.body || document.documentElement,
@@ -92,7 +88,7 @@
 	/**
 	 * Plugin.hasTouch
 	 * 检测当前浏览器是否支持触摸事件
-	 * @return `true` or `false`
+	 * @return true or false
 	 **/
 	Plugin.hasTouch = (function () {
 		if (typeof TouchEvent !== "undefined") {
@@ -101,55 +97,48 @@
 	})();
 
 
-	/*****************************************
-	 *************** 原型方法 *****************
-	 *****************************************/
 	/**
 	 * 幻灯片实例初始化函数
 	 * 
 	 **/
 	Plugin.prototype.init = function() {
 		var $el = this.el;
-		// this.animating {Boolean}
+
 		this.animating = false;
-		// this.total {Number} 幻灯片的总数量
+
+		// this.total 幻灯片的总
 		this.total = $el.children().length;
-		// this.current {Number} 当前显示的幻灯片的索引
+
+		// this.current 当前显示的幻灯片的索引
 		this.current = this.options.start - 1;
-		$el.css('overflow', 'hidden');
-		this.slides = $el.children()
-			.addClass("wslides-slide")
-			.css({
-				position: "absolute",
-				top: 0,
-				left: 0,
-				width: "100%",
-				zIndex: 0,
-				display: "none",
-				webkitBackfaceVisibility: "hidden"
-			})
-			.wrapAll('<div class="wslides-control" />');
 
-		this.slidesControl = this.slides.parent()
-			.css({
-				position: 'relative',
-				left: 0
-			})
-			.wrap('<div class="wslides-container" />');
+        // 初始化幻灯片样式
+		this.slides = $el.children().addClass("w-slider-slide").css({
+            width: "100%",
+            display: "none",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: 0
+        }).wrapAll('<div class="w-slider-control">');
 
-		this.slidesContainer = this.slidesControl.parent()
-			.css({
-				overflow: "hidden",
-				position: "relative",
-				height: '100%'
-			});
+        // 初始化幻灯片容器样式
+		this.slidesControl = this.slides.parent().css({
+            width: '100%',
+            height: this.options.height,
+            position: 'relative',
+            left: 0
+        }).wrap('<div class="w-slider-container">');
 
+		this.slidesContainer = this.slidesControl.parent().css({
+            overflow: "hidden",
+            position: "relative"
+        });
+
+        // 初始化完成,显示插件
 		$el.show();
 
-		// 初始化插件尺寸
-		this.update();
-
-		// 初始化触摸事件
+		// 初始化touch事件
 		if (Plugin.hasTouch) {
 			this.setupTouch();
 			this.slidesControl.on('touchstart', $.proxy(this.touchStart, this));
@@ -157,39 +146,35 @@
 			this.slidesControl.on('touchend', $.proxy(this.touchEnd, this));
 		}
 
-		// 初始化时，显示current图片，并设置zindex为10
-		this.slidesControl.children(":eq(" + this.current + ")").eq(0)
-			.fadeIn(0, function() {
-				$(this).css('z-index', '10');
-			});
+        // 显示当前幻灯片
+        this.slidesControl.children(":eq(" + this.current + ")").css({
+            display: 'block',
+            zIndex: '10'
+        });
 
 		// 初始化导航
-		if (this.options.navigation.active) {
-			var prevBtn, nextBtn;
-
-			// 添加prev-button及侦听其click事件
-			prevBtn = $("<a>", {"class": "wslides-previous wslides-navigation", href: "#", title: "Previous", text: "Previous"})
-				.appendTo(this.slidesContainer)
+		if (this.options.navigation) {
+            // 创建prev button
+			$("<a>", {"class": "w-slider-previous w-slider-navigation", href: "#", text: "Previous"}).appendTo(this.slidesContainer)
 				.on('click', $.proxy(function (e) {
 					this.stop(true);
 					this.previous();
 					e.preventDefault();
 				}, this));
 
-			// 添加next-button及侦听其click事件
-			nextBtn = $("<a>", {"class": "wslides-next wslides-navigation", href: "#", title: "Next", text: "Next"})
-				.appendTo(this.slidesContainer)
+            // 创建next button
+			$("<a>", {"class": "w-slider-next w-slider-navigation", href: "#", text: "Next"}).appendTo(this.slidesContainer)
 				.on('click', $.proxy(function (e) {
-					this.stop(true);
-					this.next();
-					e.preventDefault();
+                    this.stop(true);
+                    this.next();
+                    e.preventDefault();
 				}, this));
 		}
 
 		// 初始化自动播放功能
 		if (this.options.play.active) {
 			// 添加play-button及侦听其click事件
-			this.playBtn = $("<a>", {"class": "wslides-play wslides-navigation", href: "#", title: "Play", text: "Play"})
+			this.playBtn = $("<a>", {"class": "w-slider-play w-slider-navigation", href: "#", title: "Play", text: "Play"})
 				.appendTo($el)
 				.on('click', $.proxy(function (e) {
 					this.play(true);
@@ -197,7 +182,7 @@
 				}, this));
 
 			// 添加stop-button及侦听其click事件
-			this.stopBtn = $("<a>", {"class": "wslides-stop wslides-navigation", href: "#", title: "Stop", text: "Stop"})
+			this.stopBtn = $("<a>", {"class": "w-slider-stop w-slider-navigation", href: "#", title: "Stop", text: "Stop"})
 				.appendTo($el)
 				.on('click', $.proxy(function (e) {
 					this.stop(true);
@@ -212,39 +197,32 @@
 		}
 
 		// 初始化分页
-		if (this.options.pagination.active) {
-			this.pagination = $("<ul>", {"class": "wslides-pagination"})
-				.appendTo($el);
+		if (this.options.pagination) {
+			this.pagination = $("<ul>", {"class": "w-slider-pagination"}).appendTo($el);
 
 			for (var i = 0, l = this.total; i < l; i++) {
 				var paginationItem, paginationLink;
 
-				paginationItem = $("<li>", {"class": "wslides-pagination-item"})
-					.appendTo(this.pagination);
+				paginationItem = $("<li>", {"class": "w-slider-pagination-item"}).appendTo(this.pagination);
 
-				paginationLink = $("<a>", {"href": "#", "wslides-item": i, html: i + 1})
-					.appendTo(paginationItem)
+				paginationLink = $("<a>", {"href": "#", "wslides-item": i, html: i + 1}).appendTo(paginationItem)
 					.on('click', $.proxy(function (e) {
 						this.stop(true);
-						this.goto(+e.currentTarget.getAttribute("wslides-item") + 1);
+						this.goto(+e.currentTarget.getAttribute("w-slider-item") + 1);
 						e.preventDefault();
 					}, this));
 			}
-			// 设置pagination active样式
+
+			// 设置当前页状态
 			this.setPaginationActive();
 		}
 
-		// 注册window resize事件
-		$(window).on("resize", $.proxy(function () {
-			this.update();
-		}, this));
-
-		// 插件加载完毕后是否自动播放
+		// 是否自动播放
 		if (this.options.play.auto) {
 			this.play();
 		}
 
-		// 插件加载完毕后执行loaded回调
+		// 插件加载完毕,执行loaded回调
 		this.options.callback.loaded(this.options.start);
 	};
 
@@ -280,8 +258,12 @@
 	 * @param effect {String}
 	 **/
 	Plugin.prototype.next = function () {
+        setTimeout(function () {
 		this.direction = "next";
-		this.options.currentEffect === "fade" ? this.fade() : this.slide();
+
+            this.options.currentEffect === "fade" ? this.fade() : this.slide();
+        }.bind(this), 0)
+
 	};
 
 
@@ -358,8 +340,7 @@
 			left = this.slidesControl.position().left,
 			width = this.options.width;
 
-		if (left > width * 0.5 ||
-			left > width * 0.1 &&
+		if (left > width * 0.5 || left > width * 0.1 &&
 			(Number(new Date()) - this.touchtimer < 250)) {
 			this.direction = "previous";
 			this.slide();
@@ -418,7 +399,7 @@
 
 			this.playing = true;
 
-			this.playBtn.addClass("wslides-playing");
+			this.playBtn.addClass("w-slider-playing");
 
 			if (this.options.play.swap) {
 				$(".wslides-play").hide();
@@ -437,7 +418,7 @@
 		this.playInterval = null;
 		this.playing = false;
 		if (this.options.play.active) {
-			this.playBtn.removeClass("wslides-playing");
+			this.playBtn.removeClass("w-slider-playing");
 		}
 		if (this.options.play.swap) {
 			$(".wslides-stop").hide();
@@ -451,14 +432,18 @@
 	 * @param number {Number} 幻灯片的索引
 	 **/
 	Plugin.prototype.slide = function (number) {
+        //this.animating = true;
 		var currentSlide = this.current;
 		// 如果幻灯片不在运动中（即animating状态为false时），
 		// 且要移动的位置不是当前
 		if (!this.animating && number !== currentSlide + 1) {
+			//var	width = (typeof this.options.width) == 'number' ? this.options.width : this.slidesControl.width(),
 			var	width = this.options.width,
 				vector,
 				next,
 				value;
+
+
 			// 设置animating状态为true
 			this.animating = true;
 			// 如果定义了number，则向前或向后移动(number － 1)个单位
@@ -491,7 +476,8 @@
 			slidesControl.children(":eq(" + next + ")")
 				.css({
 					display: "block",
-					left: value * this.options.width,
+					//left: (typeof width == 'number') ? value * width : width,
+                    left: value > 0 ? '100%' : '-100%',
 					zIndex: 10
 				});
 
@@ -504,28 +490,50 @@
 					duration = Plugin.vendorPrefix + "TransitionDuration";
 				// 设置幻灯片的Transform和TransitionDuration
 				// 设置transition动画属性
-				slidesControl[0].style[transform] = "translateX(" + vector + "px)"; // @direction 为移动的距离
+                //console.log(vector)
+
+                //console.log(slidesControl[0])
+
+
+                var he = value > 0 ? '-100%' : '100%';
+				//slidesControl[0].style[transform] = "translateX(" + vector + "px)"; // @direction 为移动的距离
+				slidesControl[0].style[transform] = "translateX(" + he + ")"; // @direction 为移动的距离
 				slidesControl[0].style[duration] = this.options.effectOptions.slide.speed + "ms"; // @speed 为移动的速度
+
+
 				// 侦听transition end事件，结束时销毁
 				slidesControl.one("transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd", $.proxy(function() {
-					// 重置Transform和TransitionDuration
-					slidesControl[0].style[transform] = "";
-					slidesControl[0].style[duration] = "";
-					// 重置当前幻灯片的样式
-					slidesControl.children(":eq(" + next + ")")
-						.css({
-							left: 0
-						});
-						// 重置非当前幻灯片的样式
-					slidesControl.children(":not(:eq(" + next + "))")
-						.css({
-							display: "none",
-							left: 0,
-							zIndex: 0
-						});
+
+                    setTimeout(function () {
+                        // 重置Transform和TransitionDuration
+                        slidesControl[0].style[transform] = "";
+                        slidesControl[0].style[duration] = "";
+                        // 重置当前幻灯片的样式
+
+
+
+                        slidesControl.children(":eq(" + next + ")")
+                            .css({
+                                display: "block",
+                                left: 0
+                            });
+
+
+                        // 重置非当前幻灯片的样式
+                        slidesControl.children(":not(:eq(" + next + "))")
+                            .css({
+                                display: "none",
+                                left: 0,
+                                zIndex: 0
+                            });
+
+                        this.animating = false;
+                    }.bind(this), 0)
+
+
+
 				}, this));
 
-			// 否则使用jquery动画
 			} else {
 				slidesControl.stop().animate({
 					left: direction
@@ -547,7 +555,7 @@
 			// 更新current属性
 			this.current = next;
 			// 更新animating为false
-			this.animating = false;
+			//this.animating = false;
 			// 重置touch事件
 			if (Plugin.hasTouch) {
 				this.setupTouch();
@@ -610,7 +618,7 @@
 			this.options.callback.start(currentSlide + 1);
 
 			// 如果淡入淡出动画有过渡效果
-			if (this.options.effectOptions.fade.crossfade) {
+			if (this.options.effectOptions.fade.crossFade) {
 				// 执行过渡动画，fadeOut当前幻灯片
 				slidesControl.children(":eq(" + this.current + ")")
 					.stop()
